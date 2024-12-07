ee88dcd3252de1c6119e6e900f7be1717d4b9c5d
HumafahadSidde <fhuma302@gmail.com>
2024-12-07 20:10:24 +0500

    first commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,21 @@ export default {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      screens:{ 
+        "xs": "480px",
+        "sm": "640px",
+        "md": "768px",
+        "lg": "1024px",
+        "xl": "1280px",
+        "2xl": "1536px",
+        "custom":"1400px"
+
+    },
+    backgroundImage:{
+  //  bannerImg:"url("/Rectangle2.png")",
+   blackOverlay:"linear-gradient( to botton,180deg, rgba(0, 0, 0, 0.00) 0%, rgba(0, 0, 0, 0.85) 100%)",
+    }
+    
     },
   },
   plugins: [],
